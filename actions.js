d32f2dd2befd222f7cd845b69c0ebe9efab2b19e
Allan Houston <allan.houston@dimensiondata.com>
2013-07-31 16:30:58 +0200

    Added name to a href in dropdown

--- actions.js
+++ actions.js
@@ -5,11 +5,18 @@ function doActions() {
         // | Close faults with various codes | 
         //  ---------------------------------
    
-     $("a[name^='timecheck_']").click(function() {
+     $("a[name^='timecheck_']").each(function() { 
+	
+	var thisbutton = $(this);
+	console.log("Adding handler:");
+	console.log(thisbutton);
+
+	thisbutton.click(function() {
  
-	$this = $(this);
-	var monthClicked =$this.id();
-	alert(monthClicked); 
+		var thisthis = $(this);
+		var monthClicked =thisthis.id();
+		alert(monthClicked); 
+	});
 
       });
     
