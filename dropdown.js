94800c6c4791c52477ce80edd5dde7dedaeaad50
Allan Houston <allan.houston@dimensiondata.com>
2013-07-31 14:56:16 +0200

    Added dropdown code

---
+++ dropdown.js
@@ -0,0 +1,22 @@
+function getDropDown(incidentRequest) {
+
+        var newButton = "";
+
+        newButton += '<div id="split_button_div" style="width: 280px; display: none;"> ' +
+        '<div> ' +
+        '<button id="refresh" style="background-image:url(https://ahouston.net/js/css/smoothness/images/refresh.png?moo=1213); background-repeat:no-repeat;">&nbsp; &nbsp;</button> ' +
+        '<button id="rerun" disabled style="opacity: 1;">Actions</button> ' +
+        '<button id="select">Select an action</button> ' +
+                '</div> ' +
+                '<ul> ';
+
+
+         newButton += '' +
+		'  <li><a id="check_july" href="#">Check: July 2013</a></li> ' +
+                ' <li><hr style=" color:#000000; border: 1px #000000;  height:1px; width:350px;"></li> ';
+
+    newButton +=   '</ul> ' + '</div>';
+    return(newButton);
+}
+
+
