aa92cdaf28bc5f858d7bef37c63e99ea0505d5aa
Allan Houston <allan.houston@dimensiondata.com>
2013-08-27 14:14:49 +0200

    Console logging to setRows()

--- functions.js
+++ functions.js
@@ -12,6 +12,9 @@
 function setRows(rows) {
 
     var list = unsafeWindow.GlideList2.get('task_time_worked');
+
+    console.log("This is GlideList2:");
+    console.log(list);   
 	
     try { var rowsPerPage = list.rowsPerPage; }   // Occasion undefined error - try/catch should work around for the moment
     catch(err) {
