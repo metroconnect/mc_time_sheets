ed19eec6e4f00c95d5eeed5eaae16f17533cc3af
Allan Houston <allan.houston@dimensiondata.com>
2013-08-29 08:48:41 +0200

    Testing hooks

--- timesheets.js
+++ timesheets.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name       MetroConnect Timesheets
 // @namespace  https://github.com/metroconnect/mc_glist
-// @version    2.0.1
+// @version    2.0.2
 // @require    https://raw.github.com/metroconnect/mc_time_sheets/master/jquery.min.js
 // @require    https://raw.github.com/metroconnect/mc_time_sheets/master/calendar.js?moo1444
 // @require    https://raw.github.com/metroconnect/mc_time_sheets/master/jquery-ui.js
@@ -12,6 +12,8 @@
 // @description MetroConnect ServiceNow Actions
 // @include    https://didataservices.service-now.com/task_time_worked_list.do*
 // @include    https://didataservices.service-now.com/task_time_worked.do?&mc_time_sheets=1*
+// @updateURL  https://raw.github.com/metroconnect/mc_time_sheets/master/metadata.js
+// @downloadURL https://raw.github.com/metroconnect/mc_time_sheets/master/servicenow.js
 // @copyright  2013, Allan Houston
 // ==/UserScript==
 
